f8a28f47f5957f09e8176172e4b190640007f143
Kevin Langat <kevnlan11@gmail.com>
2021-05-25 10:56:54 +0300

    Heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn demo_e_commerce.wsgi --log-file -
--- demo_e_commerce/settings.py
+++ demo_e_commerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-a5*zz2p6=d&m!o
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://kevin-buy-n-sell.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
